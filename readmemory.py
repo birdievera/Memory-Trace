f4aa3fee59ab3a7bad1d7ad554124234602c4a7b
Sipicki Vera <c3sipick@cdf.toronto.edu>
2015-10-30 23:36:19 -0400

    Fixed bugs

--- readmemory.py
+++ readmemory.py
@@ -5,14 +5,18 @@ def trace(filename, output):
     outputfile = open(output, "w")
     ipages = dict() #stores instruction accesses
     dpages = dict() #stores data accesses
+    totali = 0
+    totald = 0
     
     # get all occurances of S, M, L and I accesses
     for line in inputfile:
-        if line.split[1].upper() == "I":
+        if line.split(",")[1][0].upper() == "I":
             ipages[line[:-3]] = ipages.get(line[:-3], 0) + 1
-        elif line.split[1].upper() == "S" or line.split[1].upper() == "M" or \
-             line.split[1].upper() == "L":
+            totali += 1
+        elif line.split(",")[1][0].upper() == "S" or line.split(",")[1][0].upper() == "M" or \
+             line.split(",")[1][0].upper() == "L":
             dpages[line[:-3]] = dpages.get(line[:-3], 0) + 1
+            totald+=1
         
     outputfile.write("Instructions\n")
     # write the page table into a new file
@@ -21,11 +25,12 @@ def trace(filename, output):
     outputfile.write("Data\n")
     for key in dpages:
         outputfile.write(str(key) + ", " + str(dpages[key]) + "\n")        
-    
+    outputfile.write("Total Instructions:" + str(totali) + "\nTotal Data:" + str(totald) + "\n")
     # close both files
     inputfile.close()
     outputfile.close()
 
 
 if __name__ == "__main__":
-    trace ("text.txt", "output_table.txt")
+    trace ("tr-heaploop.ref", "output_table.txt")
+    trace ("tr-matmul.ref", "output_table_m.txt")
